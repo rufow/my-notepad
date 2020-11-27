5b3c4ba9742a476aae5374cb18ba4a9171b30383
Sergej Rufov <29354876+rufow@users.noreply.github.com>
2020-11-27 20:48:08 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from cx_Freeze import setup, Executable
 
 setup(
-    name = "Блокнот",
-    version = "0.1",
-    description = "Блокнот, который ничего не забудет",
-    author = "Сергей Руфов",
-    author_email = "rufov@freeun.ru",
-    options = {"build_exe": {"include_files": ["favicon.ico", "favicon.png"]}},
-    executables = [Executable(script = "main.py", icon = "favicon.ico", targetName = "notepad.exe")]
+    name = "Блокнот", # Имя программы
+    version = "0.1", # Версия программы
+    description = "Блокнот, который ничего не забудет", # Краткое описание о программе
+    author = "Сергей Руфов", # Автор
+    author_email = "rufov@freeun.ru", # Адрес email автора
+    options = {"build_exe": {"include_files": ["favicon.ico", "favicon.png"]}}, # Подключение нужно для того чтобы в папке программы оказались иконки программы
+    executables = [Executable(script = "main.py", icon = "favicon.ico", targetName = "notepad.exe")] # Информация о сборке
 )
